ec6cc87aab04e4ccc6f52be8be5c6a4dfa5a0d46
fxrcha <truetype24@gmail.com>
2020-05-15 01:34:51 +0900

    벡엔드 디버깅, 수정 필요

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+import orjson
+import logging
+import sys
+from flask import Flask, render_template, request
 app = Flask (__name__)
+
+def getDevice (model):
+    with open("device.json") as json :
+        data = orjson.loads(json)
+    returnValue = data[model.upper]
+    return returnValue
 
 
 @app.route('/')
 def main () :
-    return render_template("main.html")
+    return render_template("index.html")
+
+@app.route('/download', methods = ['POST', 'GET'])
+def device () :
+    if request.method == "POST":
+        try:
+            try:
+                form = request.form
+                model = form.get("model")
+                json = getDevice(model)
+                sys.stdout.flush(model)
+            except (IndexError, KeyError):
+                return "."
+        except Exception as e:
+            error = "{}".format(e)
+            return error
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
