a7a06dab1370f5e39d4e412aa24db7bfaa97d422
fxrcha <truetype24@gmail.com>
2020-05-14 11:37:39 +0900

    프론트엔드 작업 - 원본 HTML : Yuda

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask (__name__)
+
 
 @app.route('/')
 def main () :
-    return "<h1>Hello Vega!</h1>"
+    return render_template("main.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
